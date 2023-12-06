ce2e4369926874ab452ddf643bce6d1060c17ea1
HazE <ezedinnuru85@gmail.com>
2023-12-06 09:44:55 +0300

    b

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import ReactGA from "react-ga4";
+
+ReactGA.initialize("G-3LGZR0DZP9");
+
+ReactGA.send({ hitType: "pageview", page: window.location.pathname });
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
